8cf25a88c7b9927faba2f419be3771d8967d97b1
Juan Fonseca <110582471+juanfonseca09@users.noreply.github.com>
2023-07-27 18:17:23 -0300

    sd

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,23 @@ import { Galeria } from './components/Galeria'
 import { Inicio } from './components/Inicio'
 import { Propuesta } from './components/Propuesta'
 import { HeaderNav } from './components/HeaderNav'
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
 
 function App() {
   return (
     <>
+      <FloatingWhatsApp
+        phoneNumber="598092037009"
+        accountName="Agrupación Purificación"
+        statusMessage="Responderemos a la brevedad"
+        avatar="./logowp.png"
+        chatMessage="Hola, en que podemos ayudarte?"
+        placeholder="Escribe tu mensaje..."
+        allowEsc
+        allowClickAway
+        notification
+        notificationSound
+      />
       <HeaderNav/>
       <Inicio/>
       <Elsa/>
--- src/components/Propuesta.jsx
+++ src/components/Propuesta.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Propuesta.css";
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import { Parallax } from "react-parallax";
 
 export const Propuesta = () => {
@@ -14,7 +14,9 @@ export const Propuesta = () => {
       <p className="p-4">
         Existen asuntos pendientes y desafíos emergentes que plantean preguntas al país, debido a errores pasados y problemas persistentes que la evolución de nuestra realidad ha puesto al descubierto. Para ambos escenarios, se hace necesario implementar políticas integrales de estado con la participación activa de todos los actores involucrados en cada proceso. Junto a ello, se requiere una nueva visión en las políticas públicas, destacando la importancia de la transversalidad sistémica, enfocada en proyectos y programas a largo plazo. Enfatizamos la importancia de articular medidas que impacten simultáneamente en los principales objetivos que se buscan alcanzar, ya que estos están estrechamente interconectados. Nuestra meta es lograr una sociedad más cohesionada y adaptada a los desafíos venideros, tomando en cuenta las lecciones aprendidas y enfrentando los problemas con determinación y colaboración. Solo así podremos forjar un futuro próspero y sostenible para todos los ciudadanos.
       </p>
-      <button type="button" className="btn btn-outline-primary col-3 py-3">Leer Más</button>
+      <Button className='col-6 py-2' variant="outline-primary" type="submit">
+        Leer Mas
+      </Button>
     </div>
   </Container>
 </div>
