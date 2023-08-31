98f873dd324a85373412aadb98801f365a21faad
Juan Fonseca <110582471+juanfonseca09@users.noreply.github.com>
2023-08-30 22:00:09 -0300

    Update Inicio.jsx

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -12,8 +12,8 @@ export const Inicio = () => {
             <Container>
                 <div className='row col-md-6  justify-content-center'>
                     <h2 className='display-2'>Agrupación<br/>Purificación</h2>
-                    <span className='lead pb-3'>Elsa Capillera</span>
-                    <p className='small py-3'><em>"Nuestras acciones demuestran nuestro compromiso."</em></p>
+                    <span className='h3 pb-3'>Elsa Capillera</span>
+                    <p className='h5 py-3'><em>"Nuestras acciones demuestran nuestro compromiso."</em></p>
                 </div> 
                 <div className="col-3">
                 <Button href='https://parlamento.gub.uy/camarasycomisiones/legisladores/12662/iniciativas-legislador?Fechadesde=2020-02-15&Fechahasta=2025-02-14' target="_blank" className='py-2' variant="outline-danger" size="lg" type="submit">
